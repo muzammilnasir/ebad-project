b6f9442b98f5a68a14369259a88803a57eb22688
muzammilnasir <muzammilnasir1304@gmail.com>
2024-08-18 08:48:15 -0700

    resolve bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Banner from '../components/home/Banner'
-import SliderCart from '../components/home/sliderCart'
+import SliderCart from '<div styleName={} />
+<components />
+<home />
+<S></S>liderCart'
 import Bestsellers from '../components/home/Bestsellers'
 
 function Home() {

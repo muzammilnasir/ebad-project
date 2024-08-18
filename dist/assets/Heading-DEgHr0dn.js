e2e66c80dcd887aeda2cd1af14f3ff6e4e872f71
muzammilnasir <muzammilnasir1304@gmail.com>
2024-08-18 07:56:40 -0700

    add more funcality

---
+++ dist/assets/Heading-DEgHr0dn.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D8_IAuuT.js";const r=({heading:t})=>e.jsx("div",{className:"text-blue-900 dark:text-white text-2xl md:text-4xl font-bold text-center my-[30px]",children:t});export{r as H};
